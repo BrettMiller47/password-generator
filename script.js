3d837a14c7d52958e68c5d4ea9f5357805571c2a
Brett Miller <brett.miller47@yahoo.com>
2022-08-04 13:47:10 -0400

    IMPORTANT NOTE: design pivoted to utilize an object declared as password
    1) deckared global object password with properties based on user-decided password requirements
    2) moved global variables minLen & maxLen into the global object password{}
    3) replaced all references to minLen & maxLen to password.minLen & password.maxLen respectively
    4) added confirm() variables for all password requirements to generatePassword()
    5) added logical if-else statements to set the global object (passsword) properties

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-// Global Variables
-  const minLen = 8;
-  const maxLen = 128;
-
+// Global Object
+var password = {
+  minLen: 8,
+  maxLen: 128,
+  requiresLowercase: true,
+  requiresUppercase: true,
+  requiresNumerical: true,
+  requiresSpecial: true,
+};
+  
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -21,16 +27,43 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
   // Prompt for desired password length until the length is an acceptable input value
   do {
-    var desiredPassLen = prompt("How long will your password be? (min " + minLen + ", max " + maxLen + ")");
+    var desiredPassLen = prompt("How long will your password be? (min " + password.minLen + ", max " + password.maxLen + ")");
   }
   while (!isAcceptableLength(desiredPassLen));
 
-  // Prompt for boolean requirement for lowercase character(s)
-  var requiresLowercaase = confirm("Is it okay to include lowercase characters? (a, b, c, etc.)");
-  console.log(requiresLowercaase);
-  // Prompt for boolean requirement for Uppercase character(s)
-  // Prompt for boolean requirement for Numberical character(s)
-  // Prompt for boolean requirement for Special character(s)
+  // Confirm boolean requirement for lowercase character(s)
+  var confirmLowercase = confirm("Is it okay to include lowercase characters? (a, b, c, etc.) If not, press Cancel.");
+  if (confirmLowercase) {
+    password.requiresLowercase = true;
+  } else {
+    password.requiresLowercase = false;
+  }
+
+  // Confirm boolean requirement for uppercase character(s)
+  var confirmUppercase = confirm("Is it okay to include uppercase characters? (A, B, C, etc.) If not, press Cancel.");
+  if (confirmUppercase) {
+    password.requiresUppercase = true;
+  } else {
+    password.requiresUppercase = false;
+  }
+
+  // Confirm boolean requirement for numerical character(s)
+  var confirmNumerical = confirm("Is it okay to include numerical characters? (1, 2, 3, etc.) If not, press Cancel.");
+  if (confirmNumerical) {
+    password.requiresNumerical = true;
+  } else {
+    password.requiresNumerical = false;
+  }
+  console.log(password.requiresNumerical);
+
+  // Confirm boolean requirement for special character(s)
+  var confirmSpecial = confirm("Is it okay to include special characters? (?, $, #, etc.) If not, press Cancel.");
+  if (confirmSpecial) {
+    password.requiresSpecial = true;
+  } else {
+    password.requiresSpecial = false;
+  }
+  console.log(password.requiresSpecial);
   
   // Use a for loop to construct a password of length desiredPassLen while satisfying character type requirements
 
@@ -39,7 +72,7 @@ function generatePassword() {
 
 // Return a boolean indicating whether the parameter (number) is acceptable
 function isAcceptableLength(num) {
-  if ((num >= minLen) && (num <= maxLen)) {
+  if ((num >= password.minLen) && (num <= password.maxLen)) {
     return true;
   } else {
     return false;
