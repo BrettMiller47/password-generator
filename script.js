d76ed35b974637e7d67d793b4d8e1e237fd8309a
Brett Miller <brett.miller47@yahoo.com>
2022-08-05 14:04:06 -0400

    1) changed confirm prompts' wording 2) set up logical to alert the user if they didn't allow any character types 3) changed from do-while loop to if-returnAlert for length boundary errors

--- script.js
+++ script.js
@@ -37,51 +37,54 @@ generateBtn.addEventListener("click", writePassword);
 // Generate and return a password string according to global requirements
 function generatePassword() {
   // Confirm boolean requirement for lowercase character(s)
-  var confirmLowercase = confirm("Is it okay to include lowercase characters? (a, b, c, etc.) If not, press Cancel.");
+  var confirmLowercase = confirm("Allow lowercase characters? (a, b, c, etc.) If not, press Cancel.");
   if (confirmLowercase) {
     PASSWORD.requiresLowercase = true;
   } else {
     PASSWORD.requiresLowercase = false;
   }
   // Confirm boolean requirement for uppercase character(s)
-  var confirmUppercase = confirm("Is it okay to include uppercase characters? (A, B, C, etc.) If not, press Cancel.");
+  var confirmUppercase = confirm("Allow uppercase characters? (A, B, C, etc.) If not, press Cancel.");
   if (confirmUppercase) {
     PASSWORD.requiresUppercase = true;
   } else {
     PASSWORD.requiresUppercase = false;
   }
   // Confirm boolean requirement for numerical character(s)
-  var confirmNumerical = confirm("Is it okay to include numerical characters? (1, 2, 3, etc.) If not, press Cancel.");
+  var confirmNumerical = confirm("Allow numerical characters? (1, 2, 3, etc.) If not, press Cancel.");
   if (confirmNumerical) {
     PASSWORD.requiresNumerical = true;
   } else {
     PASSWORD.requiresNumerical = false;
   }
   // Confirm boolean requirement for special character(s)
-  var confirmSpecial = confirm("Is it okay to include special characters? (?, $, #, etc.) If not, press Cancel.");
+  var confirmSpecial = confirm("Allow special characters? (?, $, #, etc.) If not, press Cancel.");
   if (confirmSpecial) {
     PASSWORD.requiresSpecial = true;
   } else {
     PASSWORD.requiresSpecial = false;
   }
-  // Prompt for desired password length until the length is an acceptable input value
-  do {
-    var desiredPassLen = prompt("How long will your password be? (min " + PASSWORD.minLen + ", max " + PASSWORD.maxLen + ")");
+  // If the user declined all character types...
+  if ((confirmLowercase + confirmUppercase + confirmNumerical + confirmSpecial) == 0) {
+    return alert("Unable to generate password.  You must allow at least 1 character type.");
   }
-  while (!isAcceptableLength(desiredPassLen));
+
+  // Prompt for desired password length until the length is an acceptable input value
+  var desiredPassLen = prompt("How long will your password be? (min " + PASSWORD.minLen + ", max " + PASSWORD.maxLen + ")");
+  if (!isAcceptableLength(desiredPassLen)) {
+    return alert("Password length must be between " + PASSWORD.minLen + " - " + PASSWORD.maxLen + " characters");
+  }
+
   // Construct a password of length desiredPassLen while satisfying character type requirements
   var constructedPassword = "";
   for (i = 0; i < desiredPassLen; i++) {
-    
-    // Determine the character type and 
-
+    // Determine the character type
     // startUniRange & endUniRange must be changed to a variable value determined by character type
     // Get the random character and store in local variable newChar
     var startUniRange = 97;
     var endUniRange = 122;
     var randInt = getRandInteger(startUniRange, endUniRange);
     var newChar = String.fromCharCode(randInt);
-    
     // Add the randomly selected character (of specified character type) to the constructedPassword
     constructedPassword += newChar;
   }
